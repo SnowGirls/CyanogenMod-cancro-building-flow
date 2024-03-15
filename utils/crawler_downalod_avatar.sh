d485f9f86698ca2fdba83c195cdf8ba612dd1c56
man <ManInTheMiddle@tesla.com>
2024-03-15 19:31:26 +0800

    new version

--- utils/crawler_downalod_avatar.sh
+++ utils/crawler_downalod_avatar.sh
@@ -21,3 +21,22 @@ for ((i = 1; i <= 50; ++i)) do curl -s  https://www.qqtn.com/tx/wanghongtx_$i.ht
 for ((i = 1; i <= 144; ++i)) do curl http://www.crcz.com/touxiang/nv/list_$i.html | grep jpg | awk '{print $5}' | awk -F "=" '{print $2}' | xargs -I{} curl -O {}; done;
 for ((i = 1; i <= 144; ++i)) do curl http://www.crcz.com/touxiang/nv/hot_$i.html | grep jpg | awk '{print $5}' | awk -F "=" '{print $2}' | xargs -I{} curl -O {}; done;
 for ((i = 1; i <= 6; ++i))  do curl http://www.crcz.com/touxiang/nv/yujie/list_$i.html | grep jpg | awk '{print $5}' | awk -F "=" '{print $2}' | xargs -I{} curl -O {}; done;
+
+
+
+i.e: curl -s https://www.touxiangwu.com/touxiang/nvsheng/index_2.html | grep jpg | sed -n -e 's/^<\/a>.*\(https.*jpg\).*/\1/p' | xargs -n 1 echo "curl -O "
+for ((i = 2; i <= 960; ++i)) do curl -s https://www.touxiangwu.com/touxiang/nvsheng/index_$i.html | grep jpg | sed -n -e 's/^<\/a>.*\(https.*jpg\).*/\1/p' | xargs -n 1 curl -O ; done;
+
+
+i.e: curl -s https://www.amccaps.com/nvstx_100.html | grep jpg | sed -n -e 's/.*\(http.*jpg\).*$/\1/p'
+for ((i = 2; i <= 99; ++i)) do curl -s https://www.amccaps.com/nvstx_$i.html | grep jpg | sed -n -e 's/.*\(http.*jpg\).*$/\1/p' | xargs -n 1 curl -O ; done;
+
+
+
+
+i.e: curl -s https://www.xdn001.com/nvshengtouxiang_281.html | grep jpg | sed -n -e 's/.*\(http[^ ]*jpg\).*$/\1/p'
+for ((i = 2; i <= 280; ++i)) do curl -s https://www.xdn001.com/nvshengtouxiang_$i.html | grep jpg | sed -n -e 's/.*\(http[^ ]*jpg\).*$/\1/p' | xargs -n 1 curl -O ; done;
+
+for f in -*.jpg; do echo $f  | xargs -n 1 -I {} mv -- {} $(openssl rand -hex 17).jpg; done
+
+
